da0c75a7e0eec6c726adaf67d2717c051a87f1b2
ariel zurita <as.zuritaq@uea.edu.ec>
2025-03-28 20:54:55 -0500

    semana 15 el diccionario con informacion ficticio

---
+++ semana 15/informacion_personal.py
@@ -0,0 +1,22 @@
+# Crear el diccionario con información ficticia
+informacion_personal = {
+    "nombre": "William Zurita",
+    "edad": 55,
+    "ciudad": "Quito",
+    "profesion": "Ingeniero agronomo"
+}
+# aceder la informacion de la ciudidad y modificarla
+informacion_personal["ciudad"] = "Pichincha"
+
+# agregar uan telefono ficticio
+informacion_personal["Telefono fijo"] = 3682098
+
+#Verifica si la clave "telefono" existe en el diccionario. Si no existe, agrégala con un número de teléfono ficticio
+if "telefono" not in informacion_personal:
+    informacion_personal["telefono"] = "098886747"
+
+# eliminar informacion_persolnal (edad)
+del informacion_personal["edad"]
+
+# Imprimir el diccionario para ver el resultado
+print(informacion_personal)
